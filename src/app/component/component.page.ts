7455d0ab60fc782103f29f4a3018a1cff6cc0c35
Laboratório Informática <labinfo@unifil.br>
2019-05-22 22:07:07 -0300

    XXII/V/MMXIX: component page adjustments

--- src/app/component/component.page.ts
+++ src/app/component/component.page.ts
@@ -13,22 +13,8 @@ export class ComponentPage implements OnInit {
   ngOnInit() {
   }
 
-
-  customAlertOptions: any = {
-    header: 'Pizza Toppings',
-    subHeader: 'Select your toppings',
-    message: '$1.00 per topping',
-    translucent: true
-  };
-
-  customPopoverOptions: any = {
-    header: 'Hair Color',
-    subHeader: 'Select your hair color',
-    message: 'Only select your dominant hair color'
-  };
-
   customActionSheetOptions: any = {
-    header: 'Colors',
-    subHeader: 'Select your favorite color'
+    header: 'Linguagem atual',
+    subHeader: 'Selecione outra linguagem'
   };
 }
